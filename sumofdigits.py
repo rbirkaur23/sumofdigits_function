9fbf295d6676e0135212c072957477e916a038eb
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-25 22:38:25 +0530

    Created sumofdigits.py

---
+++ sumofdigits.py
@@ -0,0 +1,11 @@
+def total(n):
+    count=0
+    while n!=0:
+        digit=n%10
+        count=count+digit
+        n=n//10
+    print("Sum is ",count)
+#---------------------------    
+p=int(input("Enter number: ")
+total (p)
+    
